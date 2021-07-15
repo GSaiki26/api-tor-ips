d7a921a607864a7c2c387b177ecd9d20f229f7b9
GSaiki26 <GSaiki26@gmail.com>
2021-07-15 01:20:15 -0300

    Update: JS Server Running!

---
+++ API/JS/App.js
@@ -0,0 +1,12 @@
+// Libs - Uma "Divisória" para as bibliotecas
+const {exec} = require('child_process');
+const axios = require('axios');
+
+// Data
+
+// Methods - "Divisória" para todos os métodos
+function yo() {
+    console.log('yoooooo');
+}
+
+// Code - "Divisória" para todo o código que será executado.
